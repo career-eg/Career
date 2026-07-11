a963cd6fa2c823ce6eae7fe29d36b036f6b6932e
career-eg <ph.tomasabdelrady@gmail.com>
2026-07-12 01:32:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
-const CAREERK_CACHE = 'careerk-pwa-v52';
+const CAREERK_CACHE = 'careerk-pwa-v54';
+
 const CAREERK_ASSETS = [
   './',
   './index.html',
   './install.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './careerk-logo.png',
+  './careerk-logo-horizontal.png'
 ];
 
 self.addEventListener('install', event => {
@@ -16,4 +19,36 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CAREERK_CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CAREERK_CACHE).then(cache => cache.put(request, copy));
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
+});
